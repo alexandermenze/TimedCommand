6f62b4a79102928a6c7ccb9d8e2e64ac0187d5f8
Alexander Menze <a.menze@polipol.de>
2020-09-15 07:46:54 +0200

    Added method for argument parsing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace TimedCommand
 {
@@ -6,7 +7,21 @@ namespace TimedCommand
     {
         internal static async Task Main(string[] args)
         {
+            var arguments = ParseArguments(args);
+        }
 
+        private static (int delayMilliseconds, string command) ParseArguments(string[] args)
+        {
+            if (args.Length < 2)
+                throw new ArgumentException("Less than two arguments supplied!", nameof(args));
+
+            if (!int.TryParse(args[0], out var parsedDelay))
+                throw new ArgumentException("First argument is not a number!", nameof(args));
+
+            if (string.IsNullOrEmpty(args[1]))
+                throw new ArgumentException("Second argument is no valid command!", nameof(args));
+
+            return (parsedDelay, args[1]);
         }
     }
 }
